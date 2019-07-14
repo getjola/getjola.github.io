11b455c570b318735aec7e4f58121a3106c03676
Pranav Ramesh <laphatize.me@gmail.com>
2019-07-13 23:23:44 -0400

    Revamp

--- js/auth.js
+++ js/auth.js
@@ -1,32 +1,20 @@
 function login() {
-
-	console.log("Login button was clicked!")
-	var email = document.getElementById("inputEmail").value;
-	var password = document.getElementById("inputPassword").value;
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
 	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			if (this.responseText === "Invalid credentials") {
+				document.getElementById("badpass").style.display = "block";
+							document.getElementById("badpass2").style.display = "block";
+				
 
-		xhttp.onreadystatechange = function() {
-			console.log("This is being executed.")
-		
-		    if (this.readyState == 4 && this.status == 200) {
-		    	if (xhttp.responseText == "Invalid credentials") {
-		    		console.log("Invalid credentials")
-		    		document.getElementById("bad-pass").style.display = "block";
-		    	} else {
-		            console.log("Authenticated! =)")
-			        localStorage.setItem('sessid', JSON.stringify(xhttp.responseText));
-			        window.location.href = "./profile.html";
-			        window.close();
-		    	}
-
-
-		    } else {
-		    	document.getElementById("bad-pass").style.display = "block";
-		    }
-		};
-
-
-		xhttp.open("GET", `http://api1.jola.gq/sessid?username=${email}&password=${password}`, true);
-		xhttp.send();
-
+			} else {
+				localStorage.setItem('sessid', this.responseText);
+				window.location.href = "./profile.html"
+			}
+		}
+	};
+	xhttp.open("GET", `https://api1.jola.gq/sessid?email=${email}&password=${password}`, true);
+	xhttp.send();
 }
---
+++ js/getlisting.js
@@ -0,0 +1,44 @@
+function getSearchParameters() {
+      var prmstr = window.location.search.substr(1);
+      return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
+
+
+
+
+
+var params = getSearchParameters();
+params = params.name;
+var title, description, url, image1, image2, image3;
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       if (xhttp.responseText === "Bad sessid") {
+
+window.location.href = "./login.html"
+
+   } else {
+       var obj = xhttp.responseText;
+       title = obj.title;
+       description = obj.description;
+       url = obj.url;
+       image1 = obj.image1;
+       image2 = obj.image2;
+       image3 = obj.image3;
+
+   }
+    }
+};
+xhttp.open("GET", `https://api1.jola.gq/listing?name=${params}&sessid=${localStorage.getItem('sessid')}`, true);
+xhttp.send();
