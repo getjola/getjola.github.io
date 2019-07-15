9881f5e195a101b05c24359ad744b934d04784df
Pranav Ramesh <laphatize.me@gmail.com>
2019-07-14 22:41:46 -0400

    Updated CNAME and other stuff

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,5 @@
 function login() {
+	localStorage.clear()
 	var email = document.getElementById("email").value;
 	var password = document.getElementById("password").value;
 	var xhttp = new XMLHttpRequest();
@@ -10,11 +11,34 @@ function login() {
 				
 
 			} else {
+				console.log(this.responseText)
+				localStorage.setItem('email', email);
 				localStorage.setItem('sessid', this.responseText);
 				window.location.href = "./profile.html"
 			}
 		}
 	};
 	xhttp.open("GET", `https://api1.jola.gq/sessid?email=${email}&password=${password}`, true);
+	xhttp.send();
+}
+
+function register() {
+	localStorage.clear()
+		var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			if (this.responseText === "Invalid credentials") {
+				document.getElementById("badpass").style.display = "block";
+							document.getElementById("badpass2").style.display = "block";
+				
+
+			} else {
+				window.location.href = "./login.html"
+			}
+		}
+	};
+	xhttp.open("GET", `https://api1.jola.gq/createacc?email=${email}&password=${password}`, true);
 	xhttp.send();
 }
