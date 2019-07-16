68b3fc18ee735d2915f545fc4aad2958c009f1f0
Pranav Ramesh <laphatize.me@gmail.com>
2019-07-15 23:02:55 -0400

    auth updates
    really important

---
+++ js/sync.js
@@ -0,0 +1,24 @@
+var xhttp = new XMLHttpRequest();
+ var email = localStorage.getItem('email');
+  var sessid = localStorage.getItem('sessid');
+
+if (!email || !sessid) {
+          window.location.href = './login.html';
+}
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+
+       var response = xhttp.responseText;
+
+       if (response === "good") {
+                   window.location.href = './profile.html';
+       } else {
+                
+       }
+
+    }
+};
+xhttp.open("GET", `https://api1.getjola.me/checksession?sessid=${sessid}&email=${email}`, true);
+xhttp.send();
