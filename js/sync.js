c9a47ade11e759362ac340a7d3939db8950bfdcb
Pranav Ramesh <laphatize.me@gmail.com>
2019-07-16 21:50:19 -0400

    :bug: I killed a small bug where the..
    I killed a small bug where te bug kept freak reloaded login.html

--- js/sync.js
+++ js/sync.js
@@ -2,10 +2,6 @@ var xhttp = new XMLHttpRequest();
  var email = localStorage.getItem('email');
   var sessid = localStorage.getItem('sessid');
 
-if (!email || !sessid) {
-          window.location.href = './login.html';
-}
-
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
