cfdd037109ab770c9d1613efe5fdd5c8319e64d9
Pranav Ramesh <laphatize.me@gmail.com>
2019-07-20 09:59:37 -0400

    Dynamic Listings are now a thing

--- js/getLocalListings.js
+++ js/getLocalListings.js
@@ -21,6 +21,7 @@ function getListings(state) {
     if ( !state ) {
         return  "bad"
     } else {
+
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
--- js/getlisting.js
+++ js/getlisting.js
@@ -1,3 +1,5 @@
+console.log("[client] getlisting.js has loaded.");
+console.log( `https://api1.getjola.me/listing?name=${params}&sessid=${localStorage.getItem('sessid')}`)
 function getSearchParameters() {
       var prmstr = window.location.search.substr(1);
       return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
@@ -14,31 +16,37 @@ function transformToAssocArray( prmstr ) {
 }
 
 
-
-
-
 var params = getSearchParameters();
 params = params.name;
-var title, description, url, image1, image2, image3;
+var obj, title, description, url, image1, image2, image3;
 
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       if (xhttp.responseText === "Bad sessid") {
+       if (xhttp.responseText === "bad") {
+      console.log("[client] server said session id is bad");
 
-window.location.href = "./login.html"
+
+          window.location.href = "./login.html"
 
    } else {
-       var obj = xhttp.responseText;
-       title = obj.title;
-       description = obj.description;
-       url = obj.url;
-       image1 = obj.image1;
-       image2 = obj.image2;
-       image3 = obj.image3;
+      // session id was valid so run This
+
+      console.log(xhttp.responseText);
+      var card = xhttp.responseText;
+
+    try {
+      document.getElementById("company-title").innerHTML = JSON.parse(card).title;
+          document.getElementById("company-desc").innerHTML = JSON.parse(card).description;
+              document.getElementById("image1").src = JSON.parse(card).image1;
+      //
+    } catch(err) {
+      console.log(card.title);
+    }
 
    }
     }
 };
-xhttp.open("GET", `https://api1.getjola.me/listing?name=${params}&sessid=${localStorage.getItem('sessid')}`, true);
+console.log(`https://api1.getjola.me/listing?name=${params}&email=${localStorage.getItem('email')}&sessid=${localStorage.getItem('sessid')}`)
+xhttp.open("GET", `https://api1.getjola.me/listing?name=${params}&email=${localStorage.getItem('email')}&sessid=${localStorage.getItem('sessid')}`, true);
 xhttp.send();
