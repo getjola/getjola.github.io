101dab01b4b955fd493031efafa04867f01c882c
Pranav Ramesh <pranavramesh@getjola.me>
2020-02-04 08:27:03 -0500

    woot woot

--- js/listing-2020.js
+++ js/listing-2020.js
@@ -39,9 +39,11 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == "200") {
 		document.getElementById("company-title").innerHTML = getSearchParameters().title;
-		document.getElementById("company-desc").innerHTML = (xhttp.responseText).createdBy;
- x = xhttp.responseText;
- console.log(x)
+		document.getElementById("company-desc").innerHTML = (JSON.parse(this.responseText)).description;	
+		document.getElementById("author").innerHTML = (JSON.parse(this.responseText)).createdBy;
+		document.getElementById("author").href = "https://getjola.me/user?user=" + (JSON.parse(this.responseText)).createdBy;
+		console.log( (JSON.parse(this.responseText)).appURL)
+		document.getElementById("apply_button").href = "" + (JSON.parse(this.responseText)).appURL;
 	}
 }
 
