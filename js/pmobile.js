8face9821d9613eb1e6dadc9c48954ffb03db7d0
Pranav Ramesh <pranavramesh@getjola.me>
2020-02-18 01:30:09 -0500

    purge time

--- js/pmobile.js
+++
@@ -1,5 +0,0 @@
-var is_mobile = !!navigator.userAgent.match(/iphone|android|blackberry/ig) || false;
-
-if (is_mobile) {
-  window.location.href = "./m/profile.html"
-}
--- js/possibleconnections.js
+++
@@ -1,1 +0,0 @@
-// gets people will the same area code
